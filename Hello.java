77d28dac0488881f2256eb259d1dded64279028a
rawaa-web <24062@esp.mr>
2026-03-16 17:48:28 +0000

    update HelloWorld

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins CI/CD!");
+        System.out.println("Hello Jenkins Pipeline!");
     }
 }
